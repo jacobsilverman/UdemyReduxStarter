f50a62f182e6b1c19905e332921830f97f3b5178
js1 <jacob@slvr.mn>
2018-03-08 18:02:37 -0500

    done: section 1 all and section 2 up to lecture 29 todo: lectures 29-33. I didn't find a copy of this online lecture-by-lecture so i thought this could be useful for others to prevent them typing it out if you are watching on 2x speed. now you can just start following along from the point you get lost as opposed to starting from lecture 0

--- src/index.js
+++ src/index.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import SearchBar from './components/search_bar.js';
 
-import App from './components/app';
-import reducers from './reducers';
+/* 
+ * Create new component,
+ * This component should produce some HTML.
+*/
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+        videos: [],
+        selectedVideo: null
+      };
+    console.log(process.env.API_KEY2);
+    YTSearch({ key: process.env.API_KEY2, term: "surfboards" }, videos => {
+      this.setState({ videos: videos, selectedVideo: videos[0] });
+    });
+  }
+  
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+/* 
+ * Take this component's generated HTML and
+ * put it on the page (in the DOM).
+*/
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+//up to date as at s2 lecture 29
